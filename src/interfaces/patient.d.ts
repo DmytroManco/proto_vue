77cff70617a2d1d62087a0c456fc7ba02031e315
dputr <dputr@softserveinc.com>
2020-03-26 08:06:34 +0200

    Feature: update patient interface

--- src/interfaces/patient.d.ts
+++ src/interfaces/patient.d.ts
@@ -2,6 +2,11 @@ export interface IPatient {
   id: string;
   firstName: string;
   secondName: string;
+  email: string;
+  phone: string;
+  address: string;
+  address2?: string;
+  state: string;
   city: string;
   zip: number;
 }
