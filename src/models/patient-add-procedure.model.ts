7ad44cb879940e2b6de3607966acd66ae6098406
dputr <dputr@softserveinc.com>
2020-03-30 13:23:23 +0300

    Feature: add patient-add-procedure.model

---
+++ src/models/patient-add-procedure.model.ts
@@ -0,0 +1,16 @@
+// TODO: temporary
+export default class PatientAddProcedureModel {
+  title = '';
+
+  amount = 1;
+
+  date = '';
+
+  provider = '';
+
+  status = '';
+
+  insuranceCovered = false;
+
+  notes = '';
+}
