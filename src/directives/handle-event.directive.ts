1ae61f1664cea95025cfa1ecf0771524e1587008
dputr <dputr@softserveinc.com>
2020-04-06 20:12:53 +0300

    Feature: add handle-event.directive.ts

---
+++ src/directives/handle-event.directive.ts
@@ -0,0 +1,15 @@
+import { VNode } from 'vue';
+
+export const HandleEventDirective = {
+  // TODO: typing, errors
+  bind: (el: HTMLElement, binding: any, vnode: VNode) => {
+    // eslint-disable-next-line
+    const events: string[] = binding.value.events;
+    events.forEach((event) => {
+      // eslint-disable-next-line
+      vnode.componentInstance?.$on(event, (eventData: any) => {
+        binding.value.handler(event, eventData);
+      });
+    });
+  },
+};
