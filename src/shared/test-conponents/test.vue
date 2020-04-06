021990481f5b84aecda3fecdb20a69f6cfdc0734
dputr <dputr@softserveinc.com>
2020-04-06 17:47:03 +0300

    Feature: test.vue - create test component for dev purposes

---
+++ src/shared/test-conponents/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>Test Component with @Prop() Title: {{title}}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Vue,
+  Component,
+  Prop,
+} from 'vue-property-decorator';
+
+@Component({
+  name: 'test',
+})
+export default class TestComponent extends Vue {
+  @Prop({ default: '' }) title!: string;
+}
+</script>
+
+<style scoped>
+
+</style>
