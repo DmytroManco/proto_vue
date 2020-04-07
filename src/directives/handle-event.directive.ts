f0a6dcb769ce9f50621aac906faaafee87442d19
dputr <dputr@softserveinc.com>
2020-04-07 09:42:05 +0300

    Feature: handle-event.directive.ts refactor

--- src/directives/handle-event.directive.ts
+++ src/directives/handle-event.directive.ts
@@ -5,6 +5,11 @@ export const HandleEventDirective = {
   bind: (el: HTMLElement, binding: any, vnode: VNode) => {
     // eslint-disable-next-line
     const events: string[] = binding.value.events;
+
+    if (!events) {
+      return;
+    }
+
     events.forEach((event) => {
       // eslint-disable-next-line
       vnode.componentInstance?.$on(event, (eventData: any) => {
