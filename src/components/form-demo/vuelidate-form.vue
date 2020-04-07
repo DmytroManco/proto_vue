8aa6632741dd6720f12702540cb0f4b2b0130387
dputr <dputr@softserveinc.com>
2020-04-07 09:43:55 +0300

    Feature: form demo - init components

---
+++ src/components/form-demo/vuelidate-form.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Vuelidate Form Elements</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'vuelidate-form',
+})
+export default class VuelidateForm extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
