7dd9082c40ebeae7600a264f6af45d2d596053f0
dputr <dputr@softserveinc.com>
2020-04-07 09:44:30 +0300

    Feature: form demo - insert component in view

--- src/views/forms/Form.vue
+++ src/views/forms/Form.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="forms">
-    <h3>Form Examples</h3>
+    <h3>Form Examples: </h3>
+    <tabs-component :tabs="tabs"></tabs-component>
   </div>
 </template>
 
 <script lang="ts">
 
-import PatientCard from '@/components/patient/patient-card.vue';
 import { Vue, Component } from 'vue-property-decorator';
+import { Tab } from '@/shared/tabs/tab.d';
+import TabsComponent from '@/shared/tabs/tabs.vue';
 
 @Component({
   name: 'Home',
+  components: {
+    TabsComponent,
+  },
 })
 export default class FormView extends Vue {
-  mounted() {
-    this.$store.dispatch('getPatients');
-  }
+  tabs: Tab[] = [
+    {
+      title: 'Custom',
+      component: 'vanilla-form',
+    },
+    {
+      title: 'Vuelidate',
+      component: 'vuelidate-form',
+    },
+    {
+      title: 'Vee Validate',
+      component: 'vee-validate-form',
+    },
+  ];
 }
 </script>
 
