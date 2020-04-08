370d82068e3eac456ffbebe3c6aaeab71f271cdc
dputr <dputr@softserveinc.com>
2020-04-08 11:33:50 +0300

    Investigation: comment global import vee-validate

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
+// import * as rules from 'vee-validate/dist/rules';
+// import { extend, ValidationObserver, ValidationProvider } from 'vee-validate';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+// Object.keys(rules).forEach((rule) => {
+//   extend(rule, rules[rule]);
+// });
+//
+// Vue.component('ValidationObserver', ValidationObserver);
+// Vue.component('ValidationProvider', ValidationProvider);
 
 Vue.config.productionTip = false;
 
