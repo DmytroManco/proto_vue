a05904a8529729ed73c70149fde52d80c69cdcf6
dputr <dputr@softserveinc.com>
2020-04-08 12:43:34 +0300

    Store: add try/catch wrapper for requests

--- src/store/modules/patient/patients.actions.ts
+++ src/store/modules/patient/patients.actions.ts
@@ -7,31 +7,46 @@ const BASE_PATIENT_API = 'http://localhost:3000/patients';
 
 export const actions: ActionTree<PatientsStateInterface, any> = {
   async getPatients({ commit }) {
-    const response = await axios.get(BASE_PATIENT_API);
-    const patients = response.data;
-    commit('setPatientsToStore', patients);
+    try {
+      const response = await axios.get(BASE_PATIENT_API);
+      const patients = response.data;
+      commit('setPatientsToStore', patients);
+    } catch (e) {
+      throw new Error(e);
+    }
   },
   async getPatient({ commit }, id: string) {
-    const response = await axios.get(`${BASE_PATIENT_API}/${id}`);
-    const patient = response.data;
-    commit('setCurrentPatient', patient);
+    try {
+      const response = await axios.get(`${BASE_PATIENT_API}/${id}`);
+      const patient = response.data;
+      commit('setCurrentPatient', patient);
+    } catch (e) {
+      throw new Error(e);
+    }
   },
   async updatePatientInfo({ commit }, patient: PatientInterface) {
-    const request = await axios({
-      method: 'PUT',
-      url: `${BASE_PATIENT_API}/${patient.id}`,
-      data: patient,
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      }),
-    });
-
-    const response = await request.data;
-    commit('setCurrentPatient', response);
+    try {
+      const request = await axios({
+        method: 'PUT',
+        url: `${BASE_PATIENT_API}/${patient.id}`,
+        data: patient,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+      });
+      const response = await request.data;
+      commit('setCurrentPatient', response);
+    } catch (e) {
+      throw new Error(e);
+    }
   },
   async patientsServerSearch({ commit }, query: string) {
-    const response = await axios.get(`${BASE_PATIENT_API}?q=${query}`);
-    const patients = await response.data;
-    commit('setPatientsToStore', patients);
+    try {
+      const response = await axios.get(`${BASE_PATIENT_API}?q=${query}`);
+      const patients = await response.data;
+      commit('setPatientsToStore', patients);
+    } catch (e) {
+      throw new Error(e);
+    }
   },
 };
--- src/store/modules/procedures/procedures.actions.ts
+++ src/store/modules/procedures/procedures.actions.ts
@@ -6,22 +6,30 @@ const BASE_PROCEDURES_API = 'http://localhost:3000/procedures';
 
 export const actions: ActionTree<ProcedureState, any> = {
   async getPatientProceduresList({ commit }, id: string) {
-    const response = await axios.get(`${BASE_PROCEDURES_API}/${id}`);
-    const procedures = await response.data;
-    commit('setProceduresList', procedures.procedures);
+    try {
+      const response = await axios.get(`${BASE_PROCEDURES_API}/${id}`);
+      const procedures = await response.data;
+      commit('setProceduresList', procedures.procedures);
+    } catch (e) {
+      throw new Error(e);
+    }
   },
   async updatePatientProcedures({ commit }, payload) {
-    const request = await axios({
-      method: 'PUT',
-      url: `${BASE_PROCEDURES_API}/${payload.userID}`,
-      data: {
-        procedures: payload.procedures,
-      },
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      }),
-    });
-    const response = request.data;
-    commit('setProceduresList', response.procedures);
+    try {
+      const request = await axios({
+        method: 'PUT',
+        url: `${BASE_PROCEDURES_API}/${payload.userID}`,
+        data: {
+          procedures: payload.procedures,
+        },
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+      });
+      const response = request.data;
+      commit('setProceduresList', response.procedures);
+    } catch (e) {
+      throw new Error(e);
+    }
   },
 };
