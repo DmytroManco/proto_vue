09973e7697ec42b29616605d6b8a261eaefe4479
dputr <dputr@softserveinc.com>
2020-04-08 12:34:06 +0300

    Store: add axios to procedures.actions.ts

--- src/store/modules/procedures/procedures.actions.ts
+++ src/store/modules/procedures/procedures.actions.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { ActionTree } from 'vuex';
 import { ProcedureState } from './procedures.state.d';
 
@@ -5,21 +6,22 @@ const BASE_PROCEDURES_API = 'http://localhost:3000/procedures';
 
 export const actions: ActionTree<ProcedureState, any> = {
   async getPatientProceduresList({ commit }, id: string) {
-    const response = await fetch(`${BASE_PROCEDURES_API}/${id}`);
-    const procedures = await response.json();
+    const response = await axios.get(`${BASE_PROCEDURES_API}/${id}`);
+    const procedures = await response.data;
     commit('setProceduresList', procedures.procedures);
   },
   async updatePatientProcedures({ commit }, payload) {
-    const request = await fetch(`${BASE_PROCEDURES_API}/${payload.userID}`, {
+    const request = await axios({
       method: 'PUT',
-      body: JSON.stringify({
+      url: `${BASE_PROCEDURES_API}/${payload.userID}`,
+      data: {
         procedures: payload.procedures,
-      }),
+      },
       headers: new Headers({
         'Content-Type': 'application/json',
       }),
     });
-    const response = await request.json();
+    const response = request.data;
     commit('setProceduresList', response.procedures);
   },
 };
