b1fab4f2525d0b5151fc0e9272d1271e362708fe
dputr <dputr@softserveinc.com>
2020-04-09 08:59:36 +0300

    Feature: demo-form.model update model for demo

--- src/models/demo-form.model.ts
+++ src/models/demo-form.model.ts
@@ -3,8 +3,41 @@ export class DemoFormModel {
 
   email: string;
 
-  constructor(demo = { name: '', email: '' }) {
+  rules: Record<string, string>;
+
+  // TODO: for demo propose
+  options: string;
+
+  additionalControls: Record<string, string | number>;
+
+  constructor(
+    demo = { name: '', email: '' },
+    rules = { name: 'required|min:3', email: 'required|email' },
+  ) {
     this.name = demo.name;
     this.email = demo.email;
+    this.rules = rules;
+    this.options = '';
+    this.additionalControls = {
+      age: 18,
+      nickName: '',
+    };
+  }
+
+  getRules(fieldName: string): string {
+    return this.rules[fieldName];
+  }
+
+  setRules(fieldName: string, rules: string): void {
+    this.rules[fieldName] = rules;
+  }
+
+  get formModel(): Record<string, string | number> {
+    return {
+      name: this.name,
+      email: this.email,
+      options: this.options,
+      ...this.additionalControls,
+    };
   }
 }
