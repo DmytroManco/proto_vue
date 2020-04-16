7bae81d9b0edfbde1f16547f7dc83e2ab0a474d4
dputr <dputr@softserveinc.com>
2020-04-16 19:51:53 +0300

    Feature: vuetify - simple table

--- src/views/patients/Patients.vue
+++ src/views/patients/Patients.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="patients">
     <h1>This is a Patients page</h1>
-    <tabs
-      :tabs="tabs"
-      :events="['search']"
-    >
-    </tabs>
+      <v-data-table
+        :headers="headers"
+        :items="notes"
+        :disable-pagination="true"
+        :hide-default-footer="true"
+        @click:row="rowClick"
+        class="elevation-1"
+      >
+      </v-data-table>
+    <div>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -13,28 +19,47 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import Tabs from '@/shared/tabs/tabs.vue';
-import { PATIENT_SEARCH_CONSTANT } from '@/constants/search.constants';
 
 @Component({
   components: { Tabs },
 })
 export default class Patients extends Vue {
-  // TODO: play around dynamic components
-  tabs = [
+  // TODO: move to propriate file
+  headers = [
+    { text: 'Date', value: 'date' },
+    { text: 'Th', value: 'teeth' },
+    { text: 'Description', value: 'description' },
+    { text: 'ORG', value: 'organisation' },
+    { text: 'Clinic', value: 'clinic' },
+    { text: 'Provider', value: 'provider' },
+    { text: 'Status', value: 'providerStatus' },
+    { text: 'Add. Prov.', value: 'additionalProvide' },
+    { text: 'Status', value: 'additionalProvideStatus' },
+  ];
+
+  notes = [
     {
-      title: 'Tab1',
-      component: 'test',
-      props: {
-        title: 'Amazing Title',
-      },
-    },
-    {
-      title: 'Tab2',
-      component: 'search-component',
-      props: {
-        options: PATIENT_SEARCH_CONSTANT,
-      },
+      date: '03/10/2020',
+      th: '29-31',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+      organisation: 'VA',
+      clinic: 'CENTRAL',
+      provider: 'EWALK',
+      providerStatus: 'Signed',
+      additionalProvide: 'ZACKC',
+      additionalProvideStatus: 'Unsigned',
     },
   ];
+
+  // eslint-disable-next-line
+  rowClick(event, row): void {
+    row.select(true);
+  }
 }
 </script>
+
+<style lang="scss">
+  h5 {
+    font-size: $font-size-root;
+  }
+</style>
